8bddcd523fefd3f82b2283f43c6d5baf7b937178
ArtemBerezovsky <admin@mac-admin.local>
2017-05-19 14:57:32 +0300

    Initial Commit

---
+++ NSArray/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSArray
+//
+//  Created by Admin on 19/05/17.
+//  Copyright © 2017 Artem Berezovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
